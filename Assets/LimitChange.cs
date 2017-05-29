eb25b1a9c3dc22ca15fbb7ee58faa3f1c2e75c97
Diogo <diogo1243@gmail.com>
2017-05-29 12:41:44 -0300

    Made the ball launchment. Made the limit part with some bugs. Made the ball "fall". Created the GameOver object and it's script.

---
+++ Assets/LimitChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitChange : MonoBehaviour {
+	private bool isMoving;
+	private Vector2 target;
+	private bool test = true;
+
+	public float distance = 500.0f;
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		isMoving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isMoving) {
+			Camera.main.transform.Translate (Vector3.up * speed);
+			if (Vector2.Distance (new Vector2 (Camera.main.transform.position.x, Camera.main.transform.position.y), target) < 1.0f)
+				isMoving = false;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Ball") {
+			isMoving = true;
+			target = new Vector2 (Camera.main.transform.position.x, Camera.main.transform.position.y);
+			target += Vector2.up;
+			target *= distance;
+		}
+	}
+}
--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -6,25 +6,44 @@ public class BallMovement : MonoBehaviour {
 
 	public float force = 100;
 
+	private bool isStarting;
 	private Rigidbody2D rb;
 
 	void OnCollisionEnter2D (Collision2D other)
 	{
-		if (other.gameObject.CompareTag("Block") && transform.position.y > other.transform.position.y)
+		if (other.gameObject.CompareTag("Block") && transform.position.y > other.transform.position.y +0.3f)
 		{
 			rb.velocity = Vector2.zero;
+			isStarting = true;
 		}
 	}
 
 	// Use this for initialization
 	void Start () 
 	{
+		isStarting = true;
 		rb = GetComponent<Rigidbody2D> ();
-		rb.AddForce ((Vector2.up + Vector2.right*0.5f)* force);
+		rb.velocity = Vector2.zero;
+		//rb.AddForce ((Vector2.up + Vector2.right*0.5f)* force);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		if ((Input.GetMouseButtonUp (0) || Input.GetTouch(0).phase == TouchPhase.Ended) && isStarting) {
+			#if UNITY_STANDALONE
+				Vector3 direction = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				direction.z= 0.0f;
+			#endif
+			#if UNITY_ANDROID
+				Vector3 direction = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x,Input.GetTouch(0).position.y,0.0f));
+				direction.z = 0.0f;
+			#endif
+			if (direction.y > 0) {
+				direction.Normalize ();
+				rb.AddForce (new Vector2 (direction.x * force, direction.y * force));
+				isStarting = false;
+			}
+		}
 	}
 }
