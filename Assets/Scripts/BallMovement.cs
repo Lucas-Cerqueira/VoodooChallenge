412263f8b6747c706d484a07e11bad000cbd2a55
Diogo <diogo1243@gmail.com>
2017-05-29 23:16:49 -0300

    Changed the ball code so that it's impossible for the ball to get stuck.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -14,8 +14,8 @@ public class BallMovement : MonoBehaviour {
 	{
 		if (other.gameObject.CompareTag("Block") && transform.position.y > other.transform.position.y +0.3f)
 		{
-			//rb.velocity = Vector2.zero;
-			//isStarting = true;
+//			rb.velocity = Vector2.zero;
+//			isStarting = true;
 		}
 	}
 
@@ -37,6 +37,14 @@ public class BallMovement : MonoBehaviour {
 	void Update () 
 	{
 		goingUp = rb.velocity.y > 0;
+
+		launchBall ();
+
+		verifyVelocity ();
+	}
+
+	void launchBall ()
+	{
 		if (isStarting) 
 		{
 			Vector3 direction = Vector3.zero;
@@ -50,8 +58,8 @@ public class BallMovement : MonoBehaviour {
 			#if UNITY_ANDROID
 			if (Input.GetTouch(0).phase == TouchPhase.Ended)
 			{
-				direction = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x,Input.GetTouch(0).position.y,0.0f));
-				direction.z = 0.0f;
+			direction = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x,Input.GetTouch(0).position.y,0.0f));
+			direction.z = 0.0f;
 			}
 			#endif
 			if (direction.y > 0) {
@@ -59,6 +67,30 @@ public class BallMovement : MonoBehaviour {
 				rb.AddForce (new Vector2 (direction.x * force, direction.y * force));
 				isStarting = false;
 			}
+		}
+	}
+
+	void verifyVelocity ()
+	{
+		if (Mathf.Abs(rb.velocity.x) <= 0.0000000001f && rb.velocity.y != 0) {
+			Vector2 velocity = rb.velocity;
+			float multiplier = velocity.magnitude;
+			int side = Random.Range (0, 1) == 0 ? 1 : -1;
+			velocity.Normalize ();
+			velocity.y -= 0.05f;
+			velocity.x = side * 0.05f;
+			velocity *= multiplier;
+			rb.velocity = velocity;
+		}
+		if (rb.velocity.y == 0 && rb.velocity.x!=0) {
+			Vector2 velocity = rb.velocity;
+			float multiplier = velocity.magnitude;
+			int side = (Random.Range(0,1)==0?1:-1);
+			velocity.Normalize ();
+			velocity.x -= 0.001f;
+			velocity.y = side * 0.001f;
+			velocity *= multiplier;
+			rb.velocity = velocity;
 		}
 	}
 }
